206854bb5bfab4ab797fbac91c0bb5f476a8f56d
exiled674 <l.bozic@outlook.de>
2025-05-02 20:57:03 +0200

    Station und Tool Models hinzugefügt

--- DaimlerConfig/DaimlerConfig/Components/Models/Station.cs
+++ DaimlerConfig/DaimlerConfig/Components/Models/Station.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace DaimlerConfig.Components.Models
 {
-    internal class Station
+    public class Station
     {
+        private int stationID {get; set;}
+
+        private string? assemblyStation {get; set;}
+
+        private string? stationName {get; set;}
+
+        private StationType? stationType { get; set; }
+
+        private DateTime? lastModified {get; set;}
+    }
+
+    public enum StationType
+    {
+        StationController,
+        PCSService,
+        DataPump
     }
 }
