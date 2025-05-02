18e134b44086b9a79c971c157ae1916fc3aed8e2
exiled674 <l.bozic@outlook.de>
2025-05-02 21:39:40 +0200

    Die Interfaces / Klassen erben/implementieren nun

--- DaimlerConfig/DaimlerConfig/Components/Repositories/IStationRepository.cs
+++ DaimlerConfig/DaimlerConfig/Components/Repositories/IStationRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DaimlerConfig.Components.Models;
 
 namespace DaimlerConfig.Components.Repositories
 {
-    internal interface IStationRepository
+    public interface IStationRepository : IRepository<Station>
     {
     }
 }
