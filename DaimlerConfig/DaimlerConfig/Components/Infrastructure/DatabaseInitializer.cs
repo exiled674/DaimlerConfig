ee6f18279e07fb49705cab6728efdbb315819798
exiled674 <l.bozic@outlook.de>
2025-05-03 19:58:17 +0200

    Infrastructure Ordner angelegt mit der Datenbank-Anbindung. Es dient dazu, dass die Connection-Logik nur ein mal aufgerufen wird und diese Logik bei jeder CRUD-Operation einfach wiederverwendet wird + lose Kopplung (gute austauschbarkeit). Muss noch in MauiProgram cs die Singletons anlegen und dann injecten

---
+++ DaimlerConfig/DaimlerConfig/Components/Infrastructure/DatabaseInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DaimlerConfig.Components.Infrastructure
+{
+    public class DatabaseInitializer
+    {
+        private readonly IDbConnectionFactory _factory;
+
+        public DatabaseInitializer(IDbConnectionFactory factory)
+            => _factory = factory;
+
+        public void EnsureCreated()
+        {
+            using var conn = _factory.CreateConnection();
+            conn.Open();
+
+            var ddl = @"
+            PRAGMA foreign_keys = ON;
+            CREATE TABLE IF NOT EXISTS Station (
+                stationID INTEGER PRIMARY KEY AUTOINCREMENT,
+                stationNumber TEXT UNIQUE,
+                stationDescription TEXT,
+                StationType_idStationType INTEGER
+            );
+            ";
+
+            conn.Execute(ddl);
+        }
+    }
+}
