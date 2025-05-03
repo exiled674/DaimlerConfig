964d3b99729dd44f41c0e8b4ee443b5c8e9da9da
exiled674 <l.bozic@outlook.de>
2025-05-03 22:55:40 +0200

    Connection wird in Repository verwendet per dependency injection

--- DaimlerConfig/DaimlerConfig/Components/Repositories/Repository.cs
+++ DaimlerConfig/DaimlerConfig/Components/Repositories/Repository.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using DaimlerConfig.Components.Infrastructure;
+
 
 namespace DaimlerConfig.Components.Repositories
 {
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+       
+        public Repository(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
         public Task Add(TEntity entity)
         {
             throw new NotImplementedException();
