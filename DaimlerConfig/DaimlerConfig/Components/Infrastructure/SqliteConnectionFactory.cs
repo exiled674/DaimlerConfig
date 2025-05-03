9f4eea8fce7475fc339eca8d64d6ea531e1f0503
exiled674 <l.bozic@outlook.de>
2025-05-03 19:58:17 +0200

    Infrastructure Ordner angelegt mit der Datenbank-Anbindung. Es dient dazu, dass die Connection-Logik nur ein mal aufgerufen wird und diese Logik bei jeder CRUD-Operation einfach wiederverwendet wird + lose Kopplung (gute austauschbarkeit). Muss noch in MauiProgram cs die Singletons anlegen und dann injecten

---
+++ DaimlerConfig/DaimlerConfig/Components/Infrastructure/SqliteConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace DaimlerConfig.Components.Infrastructure
+{
+    public class SqliteConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqliteConnectionFactory(string databasePath)
+        {
+            _connectionString = $"Data Source={databasePath}";
+        }
+
+        public IDbConnection CreateConnection()
+            => new SqliteConnection(_connectionString);
+    }
+}
