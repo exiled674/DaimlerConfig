4cb573fcbbc4a1142fbded7d0008f87af411c444
exiled674 <l.bozic@outlook.de>
2025-05-09 11:48:15 +0200

    Einschränkungen für Station Model hinzugefügt

--- DaimlerConfig/DaimlerConfig/Components/Models/Station.cs
+++ DaimlerConfig/DaimlerConfig/Components/Models/Station.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,16 @@ namespace DaimlerConfig.Components.Models
 {
     public class Station
     {
+
         public int stationID {get; set;}
 
+        [MaxLength(10, ErrorMessage = "Maximum of 10 characters for assemblystation allowed!")]
         public string? assemblystation {get; set;}
 
+        [MaxLength(50, ErrorMessage = "Maximum of 10 characters for assemblystation allowed!")]
         public string? stationName {get; set;}
 
+        [Required(ErrorMessage = "A station type must be specified!")]
         public int StationType_stationTypeID { get; set; }
 
         public DateTime? lastModified {get; set;}
