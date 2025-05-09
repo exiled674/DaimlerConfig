df4ae8a0df22c2a811549f44b55bf91ebf78ef42
exiled674 <l.bozic@outlook.de>
2025-05-09 12:01:54 +0200

    Einschränkungen für Operatrion hinzugefügt, OperationSequence fehlt noch (falls überhaupt gebraucht)

--- DaimlerConfig/DaimlerConfig/Components/Models/Operation.cs
+++ DaimlerConfig/DaimlerConfig/Components/Models/Operation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +10,40 @@ namespace DaimlerConfig.Components.Models
     public class Operation
     {
         public int OperationID { get; set; }
-        public string OperationShortname { get; set; }
-        public string OperationDescription { get; set; }
-        public string OperationSequence { get; set; }
-        public string OperationSequenceGroup { get; set; }
-        public string OperationDecisionCriteria { get; set; }
+
+        public int ToolID { get; set; }
+
+
+
+        [MaxLength(16, ErrorMessage = "Maximum of 10 characters for operation shortname allowed!")]
+        public string? OperationShortname { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Maximum of 10 characters for operation description allowed!")]
+        public string? OperationDescription { get; set; }
+
+        // Parallel wäre 1P1, Seriell 1.1 TODO
+        public string? OperationSequence { get; set; }
+
+
+        [MaxLength(100, ErrorMessage = "Maximum of 10 characters for operation sequence group allowed!")]
+        public string? OperationSequenceGroup { get; set; }
+
+        // Ka noch
+        public string? OperationDecisionCriteria { get; set; }
+
+
         public bool AlwaysPerform { get; set; }
+
+
         public int DecisionClass { get; set; }
         public int GenerationClass { get; set; }
         public int VerificationClass { get; set; }
         public int SavingClass { get; set; }
-        public int ToolID { get; set; }
-        public bool ParallelSerial { get; set; } // z. B. "parallel" oder "serial"
+
+        public bool ParallelSerial { get; set; } 
         public bool QGateRelevant { get; set; }
+
+
         public DateTime LastModified { get; set; }
     }
 
