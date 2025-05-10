15e71296ebb81fbff782b79a92f7720b71391fb8
exiled674 <l.bozic@outlook.de>
2025-05-11 00:02:40 +0200

    Ein paar fail-tests hinzugefügt

--- DaimlerConfig/DaimlerConfigTest/StationRepositoryTest.cs
+++ DaimlerConfig/DaimlerConfigTest/StationRepositoryTest.cs
@@ -49,7 +49,7 @@ namespace DaimlerConfigTest
 
                 CREATE TABLE IF NOT EXISTS Station (
                   stationID INTEGER PRIMARY KEY AUTOINCREMENT,
-                  assemblystation TEXT NOT NULL,
+                  assemblystation TEXT NOT NULL ,
                   stationName TEXT,
                   stationTypeID INTEGER,
                   lineID INTEGER,
@@ -123,6 +123,79 @@ namespace DaimlerConfigTest
 
         }
 
+
+        [Fact]
+        public async void AddStationWithInvalidData_Fails()
+        {
+            // Versuch, eine Station mit ungültigen Daten hinzuzufügen
+            await Assert.ThrowsAsync<SqliteException>(async () =>
+            {
+                _connection.Execute(@"
+                INSERT INTO Station (assemblystation, stationName, stationTypeID, lineID)
+                VALUES (@assemblystation, @stationName, @stationTypeID, @lineID);",
+                    new
+                    {
+                        assemblystation = "InvalidStation",
+                        stationName = "InvalidStation",
+                        stationTypeID = "InvalidType", // String statt int
+                        lineID = 1
+                    });
+            });
+        }
+
+
+        /*[Fact]
+        public async void AddDuplicateStation_Fails()
+        {
+            // Erste Station hinzufügen
+            var station = new Station
+            {
+                lineID = 1,
+                assemblystation = "DuplicateStation",
+                stationTypeID = 1
+            };
+            await stationRepository.Add(station);
+
+            // Versuch, eine Station mit demselben assemblystation-Wert hinzuzufügen
+            await Assert.ThrowsAsync<SqliteException>(async () =>
+            {
+                await stationRepository.Add(station);
+            });
+        }*/
+
+        [Fact]
+        public async void AddStationWithoutRequiredFields_Fails()
+        {
+            // Versuch, eine Station ohne assemblystation hinzuzufügen
+            await Assert.ThrowsAsync<SqliteException>(async () =>
+            {
+                var station = new Station
+                {
+                    stationTypeID = 1 // assemblystation fehlt
+                };
+                await stationRepository.Add(station);
+            });
+        }
+
+        [Fact]
+        public async void AddStationWithNonExistentStationTypeID_Fails()
+        {
+            // Versuch, eine Station mit einer nicht existierenden stationTypeID hinzuzufügen
+            await Assert.ThrowsAsync<SqliteException>(async () =>
+            {
+                var station = new Station
+                {
+                    assemblystation = "InvalidStation",
+                    stationTypeID = 9999, // Nicht existierende stationTypeID
+                    lineID = 1
+                };
+                await stationRepository.Add(station);
+            });
+        }
+
+
+
+
         [Fact]
         public async void DeleteStationTest_Works()
         {
@@ -160,7 +233,44 @@ namespace DaimlerConfigTest
             Assert.Null(deletedStation); 
         }
 
-      
+        [Fact]
+        public async void DeleteAllStations_Works()
+        {
+            // Mehrere Stationen hinzufügen
+            await stationRepository.Add(new Station { lineID = 1, assemblystation = "Station1", stationTypeID = 1 });
+            await stationRepository.Add(new Station { lineID = 1, assemblystation = "Station2", stationTypeID = 1 });
+
+            // Alle Stationen löschen
+            var allStations = await stationRepository.GetAll();
+            foreach (var station in allStations)
+            {
+                await stationRepository.Delete(station);
+            }
+
+            // Überprüfen, ob keine Stationen mehr existieren
+            var remainingStations = await stationRepository.GetAll();
+            Assert.Empty(remainingStations);
+        }
+
+
+        [Fact]
+        public async void DeleteNonExistentStation_DoesNotThrow()
+        {
+            // Station erstellen, die nicht in der Datenbank existiert
+            var station = new Station
+            {
+                stationID = 9999, // Nicht existierende ID
+                assemblystation = "NonExistentStation",
+                stationTypeID = 1
+            };
+
+            // Versuch, die Station zu löschen
+            await stationRepository.Delete(station);
+
+            // Keine Ausnahme sollte ausgelöst werden
+        }
+
+
         [Fact]
         public async void UpdateStationTest_Works()
         {
@@ -236,6 +346,27 @@ namespace DaimlerConfigTest
             Assert.Null(nullUpdatedStation.lastModified);
         }
 
+        [Fact]
+        public async void UpdateStationWithInvalidData_Fails()
+        {
+            // Station hinzufügen
+            var station = new Station
+            {
+                lineID = 1,
+                assemblystation = "UpdateInvalidStation",
+                stationTypeID = 1
+            };
+            await stationRepository.Add(station);
+
+            // Versuch, die Station mit ungültigen Daten zu aktualisieren
+            await Assert.ThrowsAsync<FormatException>(async () =>
+            {
+                station.stationTypeID = int.Parse("InvalidType"); // Ungültiger Wert
+                await stationRepository.Update(station);
+            });
+        }
+
+
 
         [Fact]
         public async void GetStationByIdTest_Works()
@@ -291,6 +422,7 @@ namespace DaimlerConfigTest
         }
 
 
+       
 
 
 
