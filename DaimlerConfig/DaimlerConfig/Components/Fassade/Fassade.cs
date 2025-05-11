1a9bbcfb05c921b3901ee7d209072874429b9d18
exiled674 <l.bozic@outlook.de>
2025-05-11 21:43:41 +0200

    .

--- DaimlerConfig/DaimlerConfig/Components/Fassade/Fassade.cs
+++ DaimlerConfig/DaimlerConfig/Components/Fassade/Fassade.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DaimlerConfig.Components.Repositories;
 
 namespace DaimlerConfig.Components.Fassade
 {
     public class Fassade
     {
+        public readonly IToolRepository ToolRepository;
+        public readonly IOperationRepository OperationRepository;
+        public readonly IStationRepository StationRepository;
+
+        public Fassade(IToolRepository toolRepository, IOperationRepository operationRepository, IStationRepository stationRepository)
+        {
+            ToolRepository = toolRepository;
+            OperationRepository = operationRepository;
+            StationRepository = stationRepository;
+        }
     }
 }
