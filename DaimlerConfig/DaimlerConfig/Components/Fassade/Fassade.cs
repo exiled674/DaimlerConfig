372fcc2d62502bbcb28951dce6f7922b9ecf332c
exiled674 <l.bozic@outlook.de>
2025-05-12 13:01:49 +0200

    man kann lines hinzufügen und die werden in der db gespeichert

--- DaimlerConfig/DaimlerConfig/Components/Fassade/Fassade.cs
+++ DaimlerConfig/DaimlerConfig/Components/Fassade/Fassade.cs
@@ -38,6 +38,11 @@ namespace DaimlerConfig.Components.Fassade
             return await LineRepository.getAllOrderedByDate();
         }
 
+        public async Task AddLine(Line line)
+        {
+            await LineRepository.Add(line);
+        }
+
     }
 
 
