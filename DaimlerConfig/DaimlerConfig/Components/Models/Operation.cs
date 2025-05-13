8e87062cbdedddcad29395a863c77a8e8116bc2b
3011357 <3011357@stud.hs-mannheim.de>
2025-05-13 22:43:25 +0200

    changes commited for merge

--- DaimlerConfig/DaimlerConfig/Components/Models/Operation.cs
+++ DaimlerConfig/DaimlerConfig/Components/Models/Operation.cs
@@ -10,11 +10,11 @@ namespace DaimlerConfig.Components.Models
     public class Operation
     {
         public int operationID { get; set; }
-        public required string operationShortname { get; set; }
-        public required string operationDescription { get; set; }
-        public required string operationSequence { get; set; }
-        public required string operationSequenceGroup { get; set; }
-        public required string operationDecisionCriteria { get; set; }
+        public required string? operationShortname { get; set; }
+        public required string? operationDescription { get; set; }
+        public required string? operationSequence { get; set; }
+        public required string? operationSequenceGroup { get; set; }
+        public required string? operationDecisionCriteria { get; set; }
         public bool alwaysPerform { get; set; }
         public int decisionClassID { get; set; }
         public int generationClassID { get; set; }
