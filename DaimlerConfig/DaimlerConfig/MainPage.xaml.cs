5368c2450dbde1f0023f980e739ae07a42839100
3011357 <3011357@stud.hs-mannheim.de>
2025-05-20 20:41:46 +0200

    fixed because of merge frontend

--- DaimlerConfig/DaimlerConfig/MainPage.xaml.cs
+++ DaimlerConfig/DaimlerConfig/MainPage.xaml.cs
@@ -6,5 +6,23 @@
         {
             InitializeComponent();
         }
+
+
+        protected override void OnAppearing()
+
+
+        {
+
+
+            base.OnAppearing();
+
+
+            this.Window.MinimumWidth = 800;
+
+
+            this.Window.MinimumHeight = 600;
+
+
+        }
     }
 }
