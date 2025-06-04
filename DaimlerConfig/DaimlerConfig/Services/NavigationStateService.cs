173157bf97dd3c95eee6f0465eab3edafab7a913
unknown <VIR@L303-23.hs-mannheim.de>
2025-06-04 22:48:56 +0200

    Home und Line (Station fehlt noch)

---
+++ DaimlerConfig/DaimlerConfig/Services/NavigationStateService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.Storage;
+
+namespace DaimlerConfig.Services
+{
+    public class NavigationStateService
+    {
+        private const string LAST_ROUTE_KEY = "LastRoute";
+        private string _currentRoute = "/";
+
+        public string CurrentRoute => _currentRoute;
+
+        public void SetCurrentRoute(string route)
+        {
+            _currentRoute = route;
+            // Route sofort in Preferences speichern
+            Preferences.Default.Set(LAST_ROUTE_KEY, route);
+        }
+
+        public string GetLastSavedRoute()
+        {
+            return Preferences.Default.Get(LAST_ROUTE_KEY, "/");
+        }
+
+        public void ClearSavedRoute()
+        {
+            Preferences.Default.Remove(LAST_ROUTE_KEY);
+        }
+    }
+}
